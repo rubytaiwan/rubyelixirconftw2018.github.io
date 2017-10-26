f6518e8e6d5aabefd7b259521d8da02fe3128ceb
Irene Chang <irene84111@gmail.com>
2017-10-26 16:28:45 +0800

    Home/News Done

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'middleman'
 gem 'middleman-autoprefixer'
 gem 'slim'
 gem 'sass'
+gem 'redcarpet'
 gem 'middleman-sprockets'
 gem 'middleman-livereload'
 # gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,5 +1,6 @@
 //= require jquery/dist/jquery.min
-//= require bootstrap-sass/assets/javascripts/bootstrap.min.js
+//= require bootstrap-sass/assets/javascripts/bootstrap/affix.js
+//= require bootstrap-sass/assets/javascripts/bootstrap/modal.js
 //= require fastclick/lib/fastclick.js
 
 var $window = $(window);
@@ -7,14 +8,39 @@ var $document = $(document);
 
 $document.ready(function () {
 
+  // Navbar
   $('#affixNav').affix({
     offset: {
       top: 100
     }
   });
 
-  // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function(){
     $('.navbar-toggle:visible').click();
+  });
+
+  // Modal URL
+  $(window.location.hash).modal('show');
+  $('[data-toggle="modal"]').click(function(){
+     window.location.hash = $(this).attr('data-target');
+  });
+
+  function revertToOriginalURL() {
+    var original = window.location.href.substr(0, window.location.href.indexOf('#'))
+    history.replaceState({}, document.title, original);
+  }
+
+  $('.modal').on('hidden.bs.modal', function () {
+      revertToOriginalURL();
+  });
+
+  // Toggle More New
+  var hidden = $('.news').hasClass('hidden')
+
+  $('#news-more').click(function(){
+    if (hidden){
+      $('.news.inactive').removeClass('hidden');
+      $(this).hide();
+    }
   });
 })
